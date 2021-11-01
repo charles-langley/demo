1076da2aeb774a86656b65735c0d042c816b55ad
Charles Langley <charles@mongercash.com>
2021-11-01 10:37:16 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://api.tuktukpatrol.com/graphql`,
+          `https://api.example.com/graphql`,
         type: {
           __all: {
             limit: 150
